03c2940350eecb52dc0ca0b03ce3632c64db818b
jgriffinwyckoff-crypto <j.griffin.wyckoff@gmail.com>
2026-04-12 21:51:49 -0400

    Update sec_bot.py

--- sec_bot.py
+++ sec_bot.py
@@ -1,34 +1,22 @@
 import requests
-import feedparser
 import time
-print("SEC filings bot started")
 
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1493059995271696539/HdsqIdmj2AgD8AtBZi6SJS--uQS3Mx9SVExT4QqQ0PCfh60SuMVFr4Gl4omrk45eMomR"
-
-seen = set()
 
 def send_to_discord(message):
     requests.post(DISCORD_WEBHOOK, json={"content": message})
 
+print("SEC bot started")
+
 while True:
-    print("Checking SEC filings...")
+    try:
+        print("Bot running...")
+        
+        # test message every 60 seconds
+        send_to_discord("🚨 SEC filings bot is running")
+        
+        time.sleep(60)
 
-    feed = feedparser.parse("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent")
-
-    for entry in feed.entries:
-
-        title = entry.title
-        link = entry.link
-
-        if link in seen:
-            continue
-
-        seen.add(link)
-
-        if "8-K" in title:
-
-            message = f"🚨 SEC Filing Alert\n{title}\n{link}"
-
-            send_to_discord(message)
-
-    time.sleep(60)
+    except Exception as e:
+        print("Error:", e)
+        time.sleep(60)
