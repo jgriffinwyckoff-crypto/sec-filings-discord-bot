993e885dc5b86f7e9a415324f0cfcf0fbf3fa510
jgriffinwyckoff-crypto <j.griffin.wyckoff@gmail.com>
2026-04-12 21:46:47 -0400

    Update sec_bot.py

--- sec_bot.py
+++ sec_bot.py
@@ -1,6 +1,7 @@
 import requests
 import feedparser
 import time
+print("SEC filings bot started")
 
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1493059995271696539/HdsqIdmj2AgD8AtBZi6SJS--uQS3Mx9SVExT4QqQ0PCfh60SuMVFr4Gl4omrk45eMomR"
 
@@ -10,6 +11,7 @@ def send_to_discord(message):
     requests.post(DISCORD_WEBHOOK, json={"content": message})
 
 while True:
+    print("Checking SEC filings...")
 
     feed = feedparser.parse("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent")
 
